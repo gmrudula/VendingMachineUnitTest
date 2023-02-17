8a288ef77dd29ea0458ac444b209752d074d2628
gmrudula <gudamrudula27@gmail.com>
2023-02-17 10:13:13 +0530

    my first commit

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("VendingMachineUnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("VendingMachineUnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a2243ac5-347d-45a8-b627-7050d252b877")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using VendingMachineProject;
+
+namespace VendingMachineUnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestCoin1()
+        {
+            VendingMachineProject.Program.ValidateEnteredMoney(0.25);
+        }
+        [TestMethod]
+        public void TestCoin2()
+        {
+            VendingMachineProject.Program.ValidateEnteredMoney(8);
+        }
+    }
+}
